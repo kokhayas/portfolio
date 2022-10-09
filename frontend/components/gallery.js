18e08a4c98647fc814e4c9340c1b8b8261e075b1
kokhayas <1234.884.koki@gmail.com>
2022-10-10 00:50:47 +0900

    utcodeのgitの記事を書いたことを追加した

--- frontend/components/gallery.js
+++ frontend/components/gallery.js
@@ -76,13 +76,31 @@ export default function Gallery() {
 							ARLISS_Newcomer_training_2020_team2 Six members of the Aerospace Engineering Department worked on a microcontroller project from November 2019 to February 2020. We used H8 microcontrollers. We used copper plate circuits. We procure components at Sengoku Densho and Akizuki Denki in Akihabara. The cans are sorted according to their color.<br/><br/>
 							Technologies used: C <br/><br/>
 							{/* <a href="https://ut-medium-xi.vercel.app/">Website: https://ut-medium-xi.vercel.app/</a><br/> */}
-							<a className="hover:text-white"  href="https://github.com/kokhayas/ut_H8_Team2>">Github: https://github.com/kokhayas/ut_H8_Team2</a>
+							<a className="hover:text-white"  href="https://github.com/kokhayas/ut_H8_Team2">Github: https://github.com/kokhayas/ut_H8_Team2</a>
 						</p>
 					</div>
 			    </a>
 			</div>
 
-
+			<div className="mb-4 overflow-hidden shadow-lg rounded-lg mx-5 cursor-pointer m-auto">
+				<a href="#" className="mt-2 w-full block h-full">
+					<Image width="30" height="30" alt="profile" src="/earth.jpg" className="mx-auto object-cover rounded-full h-40 w-40  border-2 border-white dark:border-gray-800" />
+					<div className="bg-white dark:bg-gray-800 w-full p-4">
+						<p className="text-indigo-500 text-md font-medium">
+						</p>
+						<p className="text-gray-800 dark:text-white text-xl font-medium mb-2">
+							<a href="https://learn.utcode.net/docs/browser-apps/git-github/">Created an article where you can learn about git</a>
+						</p>
+						<p className="text-gray-400 dark:text-gray-300 font-light text-md">
+							utcode-learn<br/><br/>
+							I have created an article where you can learn about git<br/><br/>
+							{/* <a href="https://ut-medium-xi.vercel.app/">Website: https://ut-medium-xi.vercel.app/</a><br/> */}
+							<a href="https://learn.utcode.net/docs/browser-apps/git-github/">Website: https://learn.utcode.net/docs/browser-apps/git-github/</a><br/>
+							<a className="hover:text-white"  href="https://github.com/ut-code/utcode-learn">Github: https://github.com/ut-code/utcode-learn</a>
+						</p>
+					</div>
+			    </a>
+			</div>
 
 
 		</div>
