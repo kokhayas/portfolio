881e3e0a231a5fb5ab125f9eef03e041abf3f599
kokhayas <1234.884.koki@gmail.com>
2022-10-13 17:56:50 +0900

    snake game

--- frontend/components/gallery.js
+++ frontend/components/gallery.js
@@ -121,6 +121,46 @@ export default function Gallery() {
 			    </a>
 			</div>
 
+					<div className="mb-4 overflow-hidden shadow-lg rounded-lg mx-5 cursor-pointer m-auto">
+				<a href="#" className="mt-2 w-full block h-full">
+					<Image width="30" height="30" alt="profile" src="/earth.jpg" className="mx-auto object-cover rounded-full h-40 w-40  border-2 border-white dark:border-gray-800" />
+					<div className="bg-white dark:bg-gray-800 w-full p-4">
+						<p className="text-indigo-500 text-md font-medium">
+						</p>
+						<p className="text-gray-800 dark:text-white text-xl font-medium mb-2">
+							<a href="https://huggingface.co/datasets/kokhayas/english-debate-motions-utds">Created Game Using FPGA</a>
+						</p>
+						<p className="text-gray-400 dark:text-gray-300 font-light text-md">
+							I created snake game using FPGA. I used Verilog to code the game.
+							<br/><br/>
+
+{/*
+							<video controls width="100%">
+							<iframe
+								width="560"
+								height="315"
+								src={"https://user-images.githubusercontent.com/58214906/168440278-577784c1-435a-4098-99d2-ac2c5749f7aa.mov"}
+								title="FPGA Game"
+								frameborder="0"
+								allowFullScreen
+								/>
+								Sorry, your browser doesn't support embedded videos.
+							</video> */}
+
+
+							    {/* <video controls width="100%">
+      <source src={"https://user-images.githubusercontent.com/58214906/168440278-577784c1-435a-4098-99d2-ac2c5749f7aa.mov"} type="video/mov" />
+      Sorry, your browser doesn't support embedded videos.
+    </video> */}
+
+							{/* <a href="https://ut-medium-xi.vercel.app/">Website: https://ut-medium-xi.vercel.app/</a><br/> */}
+							{/* <a className="hover:text-white" href="https://huggingface.co/datasets/kokhayas/english-debate-motions-utds">Website: https://huggingface.co/datasets/kokhayas/english-debate-motions-utds</a><br/> */}
+							<a className="hover:text-white"  href="https://github.com/kokhayas/fpga/tree/master">Github: https://github.com/kokhayas/fpga/tree/master</a>
+						</p>
+					</div>
+			    </a>
+			</div>
+
 		</div>
 		)
 }
